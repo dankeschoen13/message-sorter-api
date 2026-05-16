e5faa06e43518071b4b00aba0c2eecdfc81c5aae
dankeschoen13 <marcobernacer@gmail.com>
2026-05-16 15:39:27 +0800

    feat: add `handle_inbound_message` api route

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,8 +1,30 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from app.services.gemini import categorize_message
 
 main_bp = Blueprint('main', __name__)
 
+
 @main_bp.post('/api/v1/inbound-message')
-def handle_inbound_msg():
+def handle_inbound_message():
     data = request.get_json()
-    return None
+
+    # Structural Validation: Ensure the payload structure is correct
+    if not data or 'message' not in data or 'email' not in data:
+        return jsonify({"error": "Bad Request. Missing 'message' or 'email' fields."}), 400
+
+    user_email = data['email']
+    message_content = data['message']
+
+    # Content Validation: Check if the message actually contains text
+    if str(message_content).strip():
+        category = categorize_message(message_content)
+    else:
+        category = "Uncategorized"
+
+    # TODO: Add db logic here.
+
+    return jsonify({
+        "status": "success",
+        "email": user_email,
+        "assigned_category": category
+    }), 200
